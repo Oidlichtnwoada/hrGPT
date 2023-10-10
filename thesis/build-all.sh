dedbaa43ea5e95e41c0e9c5b5b9de142b468e7b2
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2023-10-10 21:20:33 +0200

    fixed build and deduplicated code

--- thesis/build-all.sh
+++ thesis/build-all.sh
@@ -15,17 +15,9 @@ pdflatex $CLASS.dtx
 # Build the vutinfth class file
 pdflatex $CLASS.ins
 
-# Build the vutinfth example document
-pdflatex $SOURCE
-bibtex   $SOURCE
-pdflatex $SOURCE
-pdflatex $SOURCE
-makeindex -t $SOURCE.glg -s $SOURCE.ist -o $SOURCE.gls $SOURCE.glo
-makeindex -t $SOURCE.alg -s $SOURCE.ist -o $SOURCE.acr $SOURCE.acn
-makeindex -t $SOURCE.ilg -o $SOURCE.ind $SOURCE.idx
-pdflatex $SOURCE
-pdflatex $SOURCE
+echo
+echo
+echo Class file compiled.
 
-echo
-echo
-echo Class file and example document compiled.
+# Build the vutinfth example document
+source build-thesis.sh
