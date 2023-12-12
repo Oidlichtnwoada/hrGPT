ad2b287499619180b3b0ba32ec43eaa6f54015e1
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2023-12-12 20:43:09 +0100

    changed function name

--- libs/utils-lib/src/lib/pdf/utils.ts
+++ libs/utils-lib/src/lib/pdf/utils.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as pdf from 'pdf-parse';
 
-export async function convertPdfFileToText(pdfFilePath: string): Promise<string> {
+export async function extractTextFromPdfFile(pdfFilePath: string): Promise<string> {
   const pdfFileBuffer = fs.readFileSync(pdfFilePath);
   const pdfFileResult = await pdf(pdfFileBuffer);
   return pdfFileResult.text;
