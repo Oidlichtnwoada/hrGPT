8e06b96b174b851949f53ae28314b0d7d0083d5c
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2023-12-17 13:37:29 +0100

    exported file

--- libs/utils-lib/src/index.ts
+++ libs/utils-lib/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/chat/api/replicate/replicate';
 export * from './lib/chat/interface/interface';
 export * from './lib/chat/wrapper/wrapper';
 export * from './lib/pdf/pdf';
+export * from './lib/requirement/extractor/extractor';
