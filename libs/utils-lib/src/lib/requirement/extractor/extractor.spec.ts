438d34330474f1174a2e64f9fa568ca52e637539
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2023-12-17 14:50:12 +0100

    fixed code

--- libs/utils-lib/src/lib/requirement/extractor/extractor.spec.ts
+++ libs/utils-lib/src/lib/requirement/extractor/extractor.spec.ts
@@ -9,24 +9,22 @@ describe('extractor', () => {
       const jobPdfFilePath = getAbsolutePathFromRelativeRepoPath(
         './libs/utils-lib/src/lib/examples/sample-job-description.pdf'
       );
-      const jobRequirements = getRequirementsFromJobDescriptionPdfFile(
+      const jobRequirements = await getRequirementsFromJobDescriptionPdfFile(
         jobPdfFilePath,
         Model.GPT_4_TURBO
       );
-      expect((await jobRequirements).workExperience.length).toStrictEqual(0);
-      expect((await jobRequirements).education.length).toStrictEqual(0);
-      expect((await jobRequirements).otherQualifications.length).toStrictEqual(
-        0
-      );
-      expect((await jobRequirements).hardSkills.length).toStrictEqual(0);
-      expect((await jobRequirements).softSkills.length).toStrictEqual(0);
-      expect((await jobRequirements).specificKnowledge.length).toStrictEqual(0);
-      expect((await jobRequirements).personalTraits.length).toStrictEqual(0);
-      expect((await jobRequirements).languages.length).toStrictEqual(0);
-      expect((await jobRequirements).travel.length).toStrictEqual(0);
-      expect((await jobRequirements).jobLocation.length).toStrictEqual(0);
-      expect((await jobRequirements).workingHours.length).toStrictEqual(0);
-      expect((await jobRequirements).physicalAbility.length).toStrictEqual(0);
+      expect(jobRequirements.workExperience.length).toStrictEqual(0);
+      expect(jobRequirements.education.length).toStrictEqual(0);
+      expect(jobRequirements.otherQualifications.length).toStrictEqual(0);
+      expect(jobRequirements.hardSkills.length).toStrictEqual(0);
+      expect(jobRequirements.softSkills.length).toStrictEqual(0);
+      expect(jobRequirements.specificKnowledge.length).toStrictEqual(0);
+      expect(jobRequirements.personalTraits.length).toStrictEqual(0);
+      expect(jobRequirements.languages.length).toStrictEqual(0);
+      expect(jobRequirements.travel.length).toStrictEqual(0);
+      expect(jobRequirements.jobLocation.length).toStrictEqual(0);
+      expect(jobRequirements.workingHours.length).toStrictEqual(0);
+      expect(jobRequirements.physicalAbility.length).toStrictEqual(0);
     },
     { timeout: 60000 }
   );
