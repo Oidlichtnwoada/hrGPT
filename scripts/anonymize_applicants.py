b841e5afbf8e0ea2708177374eee0d255dfef19c
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-01-05 16:51:47 +0100

    read and write the pdf file again

--- scripts/anonymize_applicants.py
+++ scripts/anonymize_applicants.py
@@ -1,4 +1,5 @@
 from utils import get_repo_root
+from pypdf import PdfReader, PdfWriter
 
 import pathlib
 import os
@@ -7,7 +8,12 @@ def get_applicant_document_paths() -> list[str]:
     return [os.path.abspath(x) for x in pathlib.Path(os.path.join(get_repo_root(), 'sample_screening_documents')).rglob('candidate_*.pdf') if x.is_file()]
 
 def anonymize_applicant_document(path: str) -> None:
-    pass
+    pdf_reader = PdfReader(stream=path)
+    pdf_writer = PdfWriter(clone_from=pdf_reader)
+    for page in pdf_writer.pages:
+        pass
+    pdf_writer.write(path)
+    pdf_writer.close()
 
 def main() -> None:
     for applicant_document_path in get_applicant_document_paths():
