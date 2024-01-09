a1a170f26823f6859abb3827011e25aac550357b
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-01-09 15:41:47 +0100

    added the possibility to filter by job

--- hrgpt/utils.py
+++ hrgpt/utils.py
@@ -14,7 +14,7 @@ def get_module_root_path() -> str:
     return os.path.join(get_repo_root_path(), 'hrgpt')
 
 
-def get_applicant_document_paths() -> list[str]:
+def get_applicant_document_paths(filter_job_index: int | None = None) -> list[str]:
     return [os.path.abspath(x) for x in
             pathlib.Path(os.path.join(get_repo_root_path(), 'sample_screening_documents')).rglob('candidate_*.pdf') if
-            x.is_file()]
+            x.is_file() and filter_job_index is None or f'job_{filter_job_index}' in str(x)]
