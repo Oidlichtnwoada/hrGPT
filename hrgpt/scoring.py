665f4b1c15a0e647c40a50cd6579c13333bcfb81
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-01-12 15:45:46 +0100

    added more functionality to the scoring

--- hrgpt/scoring.py
+++ hrgpt/scoring.py
@@ -1,6 +1,8 @@
+from hrgpt.extraction import get_requirements_from_job_description, DEFAULT_REQUIREMENT_TYPE_DEFINITIONS
 from hrgpt.utils import get_applicant_document_paths
 
 
 def score_applicants(job_id: int, candidate_id: int) -> None:
     for job_path, candidate_paths in get_applicant_document_paths(job_id, candidate_id).items():
-        print(job_path, candidate_paths)
+        job_requirements = get_requirements_from_job_description(job_path, DEFAULT_REQUIREMENT_TYPE_DEFINITIONS)
+        print(job_requirements)
