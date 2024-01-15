6f8913108b6b949fb6db4f9a0ed9bfb158b69c40
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-01-15 11:36:43 +0100

    deleted no more used Typescript code

--- libs/utils-lib/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/chat/api/openai/openai';
-export * from './lib/chat/api/replicate/replicate';
-export * from './lib/chat/interface/interface';
-export * from './lib/chat/wrapper/wrapper';
-export * from './lib/pdf/pdf';
-export * from './lib/requirement/extractor/extractor';
-export * from './lib/path/path';
