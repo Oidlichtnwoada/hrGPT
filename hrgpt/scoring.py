753bb24239b6762686bb55c152df64a23375a15f
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-01-19 18:33:15 +0100

    made a proper log, csv and json output

--- hrgpt/scoring.py
+++ hrgpt/scoring.py
@@ -2,10 +2,14 @@ import collections
 import concurrent.futures
 import functools
 import logging
+import os
+import pathlib
+
+import polars as pl
 
 from hrgpt.extraction import get_requirements_from_job_description, DEFAULT_REQUIREMENT_TYPE_DEFINITIONS
 from hrgpt.matcher import match_job_requirements_to_candidate_cv, DEFAULT_REQUIREMENT_TYPE_WEIGHTINGS, ApplicantMatch
-from hrgpt.utils import get_applicant_document_paths
+from hrgpt.utils import get_applicant_document_paths, dumps
 
 
 def score_applicants_for_job_path(job_path: str, candidate_paths: list[str]) -> tuple[ApplicantMatch]:
@@ -27,5 +31,32 @@ def score_applicants(job_ids: tuple[int, ...], candidate_ids: tuple[int, ...]) -
         job_path = arguments[0]
         for candidate_path, match_result in zip(arguments[1], match_results):
             result_dict[job_path][candidate_path] = match_result
-    logging.debug(result_dict)
+    create_output_files(result_dict)
     return result_dict
+
+
+def create_output_files(score_result: dict[str, dict[str, ApplicantMatch]], log_result: bool = True) -> None:
+    for job_path, match_results in score_result.items():
+        # create the result directory
+        result_directory = os.path.join(os.path.dirname(job_path), 'result')
+        os.makedirs(result_directory, exist_ok=True)
+        # make the resulting dataframe per job
+        job_df_parts = []
+        for candidate_path, match_result in match_results.items():
+            # append the result part of the applicant
+            job_df_parts.append(pl.DataFrame({
+                'candidate': [candidate_path],
+                'score': [match_result.score],
+                'promising': [match_result.promising],
+                'explanation': [match_result.explanation],
+            }))
+            # save the applicants data as json in the result directory
+            match_result_json = dumps(match_results)
+            candidate_result_path = os.path.join(result_directory, f'match_result_{pathlib.Path(candidate_path).stem}.json')
+            with open(candidate_result_path, 'w') as file:
+                file.write(match_result_json)
+        job_df = pl.concat(job_df_parts).sort('score', descending=True)
+        # save the result table in the result directory
+        job_df.write_csv(os.path.join(result_directory, 'job_match_result.csv'))
+        if log_result:
+            logging.info(f'Results for the job \'{job_path}\':\n{job_df}\n')
