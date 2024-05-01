422170764388a6f5a07b7f695137f6c79b7a21e1
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-05-01 15:00:40 +0200

    changed settings and added clear all script

---
+++ thesis/clear-all.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Delete all temporary files
+rm -rf *.acn
+rm -rf *.acr
+rm -rf *.alg
+rm -rf *.aux
+rm -rf *.bbl
+rm -rf *.bcf
+rm -rf *.blg
+rm -rf *.glg
+rm -rf *.glo
+rm -rf *.gls
+rm -rf *.ind
+rm -rf *.idx
+rm -rf *.ilg
+rm -rf *.ist
+rm -rf *.lof
+rm -rf *.lot
+rm -rf *.log
+rm -rf *.out
+rm -rf *.xml
+rm -rf *.toc
+rm -rf *.cls
+rm -rf *.hd
+
+echo
+echo
+echo All temporary files deleted.
+
+
