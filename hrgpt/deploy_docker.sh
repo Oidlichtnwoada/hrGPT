b4276961a675a01318362af1ab749461da931b45
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-05-26 23:09:41 +0200

    created deploy script

--- hrgpt/deploy_docker.sh
+++ hrgpt/deploy_docker.sh
@@ -6,5 +6,17 @@ set -e
 # build the image
 ./build_docker.sh
 
+# get the current project id
+PROJECT_ID=$(gcloud config get-value project)
+
+# tag the built image
+docker tag hrgpt:latest gcr.io/$PROJECT_ID/hrgpt:latest
+
+# configure credentials
+gcloud auth configure-docker
+
+# push the image to gcr
+docker push gcr.io/$PROJECT_ID/hrgpt:latest
+
 # deploy the image
-gcloud run deploy hrgpt --image hrgpt:latest --platform managed --region europe-west3 --allow-unauthenticated --port 80
+gcloud run deploy hrgpt --image gcr.io/$PROJECT_ID/hrgpt:latest --platform managed --region europe-west3 --allow-unauthenticated --port 80
