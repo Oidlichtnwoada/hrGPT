87583da54b9c780c8137410a61e70ba673d51059
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2024-05-26 23:25:18 +0200

    fix typo

--- hrgpt/build_docker.sh
+++ hrgpt/build_docker.sh
@@ -4,4 +4,4 @@
 set -e
 
 # build the Dockerfile
-docker build --platform linux/amd64,linux/arm64build .. -f ./Dockerfile -t hrgpt:latest
+docker build --platform linux/amd64,linux/arm64 .. -f ./Dockerfile -t hrgpt:latest
